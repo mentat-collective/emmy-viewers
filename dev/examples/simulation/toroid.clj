23a5f6d0440932e4690775c73ef431748c1c5aa7
Colin Smith <colin.smith@gmail.com>
2023-04-11 23:57:57 +0200

    Geodesics of the torus (#20)

---
+++ dev/examples/simulation/toroid.clj
@@ -0,0 +1,142 @@
+(ns examples.simulation.toroid
+  (:refer-clojure
+   :exclude [+ - * / = zero? compare
+             numerator denominator ref partial
+             infinite? abs])
+  (:require [emmy.env :as e :refer :all]
+            [emmy.mechanics.rotation :as rot]
+            [emmy.expression.compile :as xc]
+            [emmy.structure :as s]
+            [examples.expression :as d]
+            [mathbox.core :as-alias mathbox]
+            [mathbox.primitives :as-alias mb]
+            [nextjournal.clerk :as clerk]
+            [nextjournal.clerk.viewer :as-alias viewer]))
+
+;; ## Geodesics of the Torus
+;;
+;; Investigate the many interesting geodesics on the torus. This is
+;; similar to the [[examples.simulation.ellipsoid]] investigation, but
+;; here we will set gravity to zero and give our particle an initial
+;; velocity tangent to the toroidal surface and see where it will
+;; travel under the surface's intrinsic notion of free-fall.
+;;
+;; First, prepare the viewers so that all literals render with the multiviewer:
+
+(clerk/add-viewers! [d/multiviewer])
+
+;; There is such a thing as toroidal coordinates--see
+;; [Wikipedia](https://en.wikipedia.org/wiki/Toroidal_coordinates)--
+;; but for our purposes those coordinates are unintuitive, and more
+;; useful in a 3d setting than we will need here. For us, living on
+;; the surface of the torus, we will describe our position by two
+;; angles: $\phi$, the azimuth, or the angle we make around the z
+;; axis measured in the xy plane which contains the torus' central
+;; axis circle of radios $R$, and $\theta$, the angle we make obout
+;; the central axis of the torus, where zero starts at the outer
+;; equator and $\pi$ represents the inner equator. The radius of
+;; the interior is denoted by $r$. Now...
+
+
+(defn toroidal->rect [_ R r]
+  (fn [[_ [theta phi] _]]
+    (*
+     (rot/rotate-z-matrix phi)
+     (s/up (+ R (* r (cos theta)))
+           0
+           (* r (sin theta))))))
+
+;; Next, the Lagrangian:
+
+(defn T-free-particle [m]
+  (fn [[_ _ v]]
+    (* 1/2 m (square v))))
+
+(defn V-free-particle [_m]
+  (constantly 0))
+
+((T-free-particle 'm)
+ (up 't
+     (up 'theta 'phi)
+     (up 'v_theta 'v_phi)))
+
+(defn L-toroidal [m R r]
+  (comp
+   (- (T-free-particle m)
+      (V-free-particle m))
+   (F->C
+    (toroidal->rect m R r))))
+
+;; Let's have a look:
+
+(let [local (up 't
+                (up 'theta 'phi)
+                (up 'thetadot 'phidot))]
+  ((L-toroidal 1 'R 'r) local))
+
+;; I was frankly expecting a little more trigonometry in there.
+;; Maybe I did something wrong. In any case, let's proceed.
+;;
+;; Lagrange equations of motion for the torus:
+
+(clerk/with-viewer d/multiviewer
+  (let [L (L-toroidal 1 'R 'r)
+        theta (literal-function 'theta)
+        phi (literal-function 'phi)]
+    (((Lagrange-equations L) (up theta phi))
+     't)))
+
+;; Ooof. Be careful what you ask for. Time to draw a picture.
+;;
+;; To influence the trajectory, you can choose the initial elevation on the
+;; upper half-torus by manipulating $\theta_0$. Imagine at that point there is
+;; a little unit circle allowing you to select the direction to proceed from
+;; there: this is $\alpha_0$, the direction of a unit vector in
+;; $(\theta, \phi)$-space.
+
+^{::clerk/viewer
+  {:transform-fn
+   (comp clerk/mark-presented
+         (clerk/update-val
+          (fn [{:keys [L params initial-state state->xyz
+                      keys] :as m}]
+            (assoc m
+                   :L
+                   (xc/compile-state-fn
+                    (compose e/Lagrangian->state-derivative L)
+                    (mapv params keys)
+                    initial-state
+                    {:mode :js
+                     :calling-convention :primitive
+                     :generic-params? true})
+
+                   :state->xyz
+                   (xc/compile-state-fn
+                    state->xyz
+                    (mapv params keys)
+                    initial-state
+                    {:mode :js
+                     :calling-convention :primitive
+                     :generic-params? true})))))
+   :render-fn 'demo.mathbox/ToroidViewer}}
+
+(let [m 10000
+      R 2
+      r 0.5
+      theta_0 0
+      alpha_0 0]
+  {:params {:mass m :R R :r r :theta_0 theta_0 :alpha_0 alpha_0}
+   :schema
+   {:R   {:min 0.5 :max 2 :step 0.01}
+    :r   {:min 0.5 :max 2 :step 0.01}
+    :theta_0 {:min 0 :max Math/PI :step 0.02}
+    :alpha_0 {:min 0 :max Math/PI :step 0.02}}
+   :keys [:mass :R :r]
+   :state->xyz toroidal->rect
+   :L L-toroidal
+   :initial-state [0 [0 0] [6 1]]
+   :cartesian
+   {:range [[-10 10]
+            [-10 10]
+            [-10 10]]
+    :scale [3 3 3]}})
--- src/demo/mathbox.cljs
+++ src/demo/mathbox.cljs
@@ -1,12 +1,17 @@
 (ns demo.mathbox
-  (:require [goog.events]
-            [goog.Timer :as timer]
-            ["odex" :as o]
-            [mathbox.primitives :as mb]
-            ["react" :as react]
-            [reagent.core :as r]
+  (:require [emmy.env :as e]
             [emmy.expression.compile :as xc]
-            [emmy.numerical.ode :as ode])
+            [emmy.mechanics.rotation :as rot]
+            [emmy.numerical.ode :as ode]
+            [goog.events]
+            [goog.Timer :as timer]
+            [leva.core]
+            [mathbox.core]
+            [mathbox.primitives :as mb]
+            [nextjournal.clerk.render]
+            ["odex" :as o]
+            ["react" :as react]
+            [reagent.core :as r])
   (:import [goog Timer]))
 
 (defn format-number [x]
@@ -43,28 +48,34 @@
                        (state-derivative ys yps parameters)))]
      (ode/stream-integrator equations 0 flat-initial-state {:js? true}))))
 
+(defn make-solver
+  [derivative initial-state parameters epsilon]
+  (let [flat-initial-state (flatten initial-state)
+         ;; TODO get around the deref
+        f' (if (implements? IAtom parameters)
+             (let [p @parameters]
+               (fn [_ ys yps]
+                 (derivative ys yps p)))
+             (fn [_ ys yps]
+               (derivative ys yps parameters)))
+        dimension (count flat-initial-state)]
+    (o/Solver.
+     f'
+     dimension
+     #js {:absoluteTolerance epsilon
+          :relativeTolerance epsilon
+          :rawFunction true
+          :maxSteps 10000})
+    )
+  )
+
 (defn Lagrangian-collector
   "hardcoded at first for this use case."
   ([state-derivative initial-state]
    (Lagrangian-collector state-derivative initial-state {}))
   ([state-derivative initial-state {:keys [parameters epsilon]
                                     :or {epsilon 1e-6}}]
-   (let [flat-initial-state (flatten initial-state)
-         ;; TODO get around the deref
-         f' (if (implements? IAtom parameters)
-              (let [p @parameters]
-                (fn [_ ys yps]
-                  (state-derivative ys yps p)))
-              (fn [_ ys yps]
-                (state-derivative ys yps parameters)))
-         dimension (count flat-initial-state)
-         solver (o/Solver.
-                 f'
-                 dimension
-                 #js {:absoluteTolerance epsilon
-                      :relativeTolerance epsilon
-                      :rawFunction true})]
-     (set! (.-denseOutput solver) true)
+   (let [solver (make-solver state-derivative initial-state parameters epsilon)]
      (fn [state n step-size emit]
        ;; TODO fix the case where we have issues at simSteps 5 on phase
        ;; portrait.
@@ -171,6 +182,36 @@
         (assoc :points "<<<"
                :colors "<"))]])
 
+(defn Curve
+  "Component that takes a simulator and builds an array of points connected
+   into a curve."
+  [{:keys [state-derivative initial-state-fn params state->xyz steps dt]
+    :or {steps 1000 dt 3e-2}}]
+  [:<>
+   [mb/Array
+    {:channels 3
+     :id "sampler"
+     :data (let [y0 (initial-state-fn)
+                 s (make-solver state-derivative y0 params 1e-5)
+                 ps (.-state params)
+                 xyz (double-array 3)
+                 pts (atom [])]
+             (.solve s 0 (clj->js y0)
+                     (* steps dt)
+                     (.grid s dt
+                            (fn [_ ys]
+                              (state->xyz ys xyz ps)
+                              (swap! pts conj (js/Array. (aget xyz 0)
+                                                         (aget xyz 2)
+                                                         (aget xyz 1))))))
+             @pts)
+     }]
+   [mb/Line
+    {:color 0xff3090
+     :size 8
+     :points "<"
+     :end true}]])
+
 (defn Comet
   "Path is a function of i, t
   dimensions is how many you want to emit
@@ -199,7 +240,6 @@
   (let [render-fn   (js/Function. a1 b1 c1 body1)
         state-deriv (js/Function. a2 b2 c2 body2)
         my-updater  (Lagrangian-updater state-deriv state {:parameters params})]
-    ;; TODO return the inner component??
     [Comet
      {:dimensions 3
       :length 16
@@ -240,6 +280,36 @@
    [mb/Surface
     {:shaded true
      :opacity 0.2
+     :lineX true
+     :lineY true
+     :points "<"
+     :color 0xffffff
+     :width 1}]])
+
+(defn Torus [render-fn !params]
+  [:<>
+   [mb/Area
+    {:width 64
+     :height 64
+     :rangeX [0 two-pi]
+     :rangeY [0 two-pi]
+     :axes [1 3]
+     :live false
+     :expr (let [in  #js [0 0 0 0 0]
+                 out #js [0 0 0]
+                 p   @!params]
+             (fn [emit theta phi _i _j _time]
+               (aset in 1 theta)
+               (aset in 2 phi)
+               (render-fn in out p)
+               (emit (aget out 0)
+                     (aget out 2)
+                     (aget out 1))))
+     :items 1
+     :channels 3}]
+   [mb/Surface
+    {:shaded true
+     :opacity 0.5
      :lineX true
      :lineY true
      :points "<"
@@ -319,3 +389,57 @@
          :color 0x3090ff
          :size 10
          :zIndex 1}]])))
+
+
+;; ## Toroid Viewer
+
+(defn ToroidViewer
+  [{state  :initial-state
+    params     :params
+    keys       :keys
+    schema     :schema
+    state->xyz :state->xyz
+    :as opts}]
+  (reagent.core/with-let
+    [render-fn (apply js/Function state->xyz)
+     !state    (reagent.core/atom {:time 0 :state state})
+     !params   (reagent.core/atom params)
+
+     ;; I had to move this here because reagent.core/reaction wasn't available
+     ;; in the SCI environment you have when writing viewers...
+     !arr      (reagent.core/reaction
+                (apply
+                 array
+                 (map @!params keys)))]
+    [:<>
+     [nextjournal.clerk.render/inspect @!arr]
+     [leva.core/Controls
+      {:atom !params
+       :schema schema}]
+     [Evolve
+      {:L      (:L opts)
+       :params !arr
+       :atom   !state}]
+     [mathbox.core/MathBox
+      {:container  {:style {:height "400px" :width "100%"}}
+       :threestrap {:plugins ["core" "controls" "cursor" "stats"]}
+       :renderer   {:background-color 0xffffff}}
+      [mb/Cartesian (:cartesian opts)
+       [mb/Axis {:axis 1 :width 3}]
+       [mb/Axis {:axis 2 :width 3}]
+       [mb/Axis {:axis 3 :width 3}]
+       [demo.mathbox/Curve
+        {:state-derivative (apply js/Function (:L opts))
+         :state->xyz render-fn
+         :initial-state-fn (fn [] (println "i-params" !params)
+                             (let [st (.-state !params)
+                                   alpha_0 (:alpha_0 st)]
+                               ;; alpha_0 is the direction of the initial velocity
+                               ;; in (theta, phi)-space. Since we're not doing dynamics,
+                               ;; the speed doesn't matter, just the direction, so we do
+                               ;; it as a unit vector.
+                               [0
+                                (:theta_0 st) 0
+                                (Math/cos alpha_0) (Math/sin alpha_0)]))
+         :params !arr}]
+       [Torus render-fn !arr]]]]))
