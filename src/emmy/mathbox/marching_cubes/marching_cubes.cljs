5cf991756c5f189453658ca5dab3b11b78eb2cab
Nobie Redmon <vnredmon@gmail.com>
2023-07-06 14:38:01 -0700

    Seems to work! gives values close to the rhs.
    ...for (TestLhs 1). But there are some points with only 2 entries. huh.
    Need to set up tests.

--- src/emmy/mathbox/marching_cubes/marching_cubes.cljs
+++ src/emmy/mathbox/marching_cubes/marching_cubes.cljs
@@ -1,18 +1,19 @@
 (ns emmy.mathbox.marching-cubes.marching-cubes
-  (:require [emmy.mathbox.marching-cubes.triangle-table :as tt
-             ]))
+  (:require [emmy.mathbox.marching-cubes.triangle-table :as tt]))
 
 (defn CubeIndex
   "Returns index into 256 value Triangle Lookup Table"
   [vertex_vector rhs]
-
   ;; TODO double check the index implementation.
   ;; compares vertex position to rhs of surface function and
   ;; returns the 8 bit number associated with that edge
-  [(reduce + (map-indexed
-              (fn [item index]
-                (* (if (> item rhs) 1 0)
-                   (Math/pow 2 index))) vertex_vector))])
+  (let [bit_exp  (map-indexed
+
+                  (fn [index item]
+                    (* (if (> item rhs) 1 0)
+                       (Math/pow 2 index)))
+                  vertex_vector)]
+    (reduce + bit_exp)))
 
 (defn interp
   [x y a b c]
@@ -37,25 +38,23 @@
       8 [x (interp y yInc v2 v16 rhs) z]
       9 [xInc (interp y (+ y yInc) v2 v32 rhs) z]
       10 [xInc (interp y yInc v4 v64 rhs)]
-      11 [(x (interp y yInc v8 v128 rhs) zInc)]
-      "Index must be between 0 and 11" ;TODO how do I throw errors in clojure?
+      11 [x (interp y yInc v8 v128 rhs) zInc]
+      (println (str "Index must be between 0 and 11. Given: " index)) ;TODO how do I throw errors in clojure?
       )))
 
-(defn GenRange [minVal maxVal resolution]
-  (map (fn [x] (+ (* x resolution) minVal))
-       (take (int (/ (- maxVal minVal) resolution)) (range))))
+(defn GenRange [minVal maxVal numPoints]
+  (map (fn [x] (+ (* x (/ (- maxVal minVal) numPoints)) minVal))
+       ;; (take (int (/ (- maxVal minVal) resolution)) (range)) ;;XXX this was wrong
+       (take numPoints (range))))
 
 (defn GenerateGrid [xMin xMax yMin yMax zMin zMax resolution]
-  (let [xVals (GenRange xMin xMax resolution)
+  (println "generating grid")
+  (let [points ()
+        xVals (GenRange xMin xMax resolution)
         yVals (GenRange yMin yMax resolution)
         zVals (GenRange zMin zMax resolution)]
-
-    ;; TODO fix for loops
-    ;; (for x xVals (for y yVals (for z zVals '(x y z)))) ; multidimensional list of permutation.
-    ;; (for [x xVals y yVals z zVals] '(x y z))  ; Flat list of permutation
-     (for [x xVals y yVals  z zVals]
-       '(x y z))
-     )) ; multidimensional list of permutation.
+    (for [x xVals y yVals  z zVals]
+      [x y z])))
 
 (defn MarchingCubesCore [lhs rhs x y z xStep yStep zStep]
   (let [v1 (lhs x y z)
@@ -67,24 +66,34 @@
         v64 (lhs (+ x xStep) (+ y yStep) (+ z zStep))
         v128 (lhs x (+ y yStep) (+ z zStep))]
 
-    (let [IsosurfaceEdgePointsPartial (partial IsosurfaceEdgePoints x y z xStep yStep zStep v1 v2 v4 v8 v16 v32 v64 v128 lhs rhs)])
-    (map IsosurfaceEdgePoints (tt/TriangleTable (CubeIndex [v1 v2 v4 v8 v16 v32 v64 v128] rhs)))))
+    (let [IsosurfaceEdgePointsPartial (partial IsosurfaceEdgePoints x y z xStep yStep zStep v1 v2 v4 v8 v16 v32 v64 v128 lhs rhs)
+          indexVal (CubeIndex [v1 v2 v4 v8 v16 v32 v64 v128] rhs)]
 
+      (for [triangle (tt/TriangleTable indexVal)]
+        (map IsosurfaceEdgePointsPartial triangle)))))
 
-;; TODO add defaults
-(defn MarchingCubes  [lhs  rhs  xMin  xMax  yMin  yMax  zMin  zMax  resolution]
-  (def points ())
-  (def xStep (GenRange xMin xMax resolution))
-  (def yStep (GenRange yMin yMax resolution))
-  (def zStep (GenRange zMin zMax resolution))
-  ;; (for (x y z) (GenerateGrid xMin xMax yMin yMax zMin zMax resolution)
-  ;; XXX: does this kind of destructuring work?
-  (for [point (GenerateGrid xMin xMax yMin yMax zMin zMax resolution)]
-    (let [x (point 1)
-          y (point 2)
-          z (point 3)]
-       (conj (MarchingCubesCore lhs rhs x y z xStep yStep zStep) points)
-       )
-    )
-  (points)
-)
+(defn MarchingCubes [lhs rhs
+                     & {:keys [xMin  xMax  yMin  yMax  zMin  zMax  resolution]
+                        :or {xMin -1  xMax 1  yMin -1  yMax 1 zMin -1 zMax 1  resolution 100}}]
+  (println "starting marching cubes")
+
+  (def xStep (/ 1 resolution))
+  (def yStep (/ 1 resolution))
+  (def zStep (/ 1 resolution))
+
+  (def grid (GenerateGrid xMin xMax yMin yMax zMin zMax resolution))
+
+  (println "stepsizes defined: " zStep)
+  (println (type grid))
+  (println "10th grid point" (nth grid 10))
+
+  (filter not-empty  ;; XXX somehow some of these only return 'points' with 2 entries....they seem to mostly evaluate to the RHS though!
+          (for [point grid]
+            (let [x (point 0)
+                  y (point 1)
+                  z (point 2)]
+              (MarchingCubesCore lhs rhs x y z xStep yStep zStep)))))
+
+(defn TestLhs [x y z]
+  (reduce +
+          (map (fn [a] (Math/pow a 2)) [x y z])))
